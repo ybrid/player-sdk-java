07fa15c02c81a6ef6e678796d840d73d7f6f5b37
Philipp Schafft <philipp.schafft@nacamar.de>
2020-11-12 12:34:30 +0000

    Feature: Added a template for a Decoder based on a Stream

---
+++ src/main/java/io/ybrid/player/io/decoder/StreamDecoder.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.player.io.decoder;
+
+import io.ybrid.player.Decoder;
+import io.ybrid.player.io.muxer.Stream;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public abstract class StreamDecoder<T extends Stream<?, ?, ?, ?>> implements Decoder {
+    protected final @NotNull T stream;
+
+    protected StreamDecoder(@NotNull T stream) {
+        this.stream = stream;
+    }
+
+    @Override
+    public boolean isValid() {
+        return stream.isValid();
+    }
+
+    @Override
+    public void close() throws IOException {
+        stream.close();
+    }
+}
