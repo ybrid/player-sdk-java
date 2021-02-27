6ae1533aa4a2263b80a5262d226ad167f2737871
Philipp Schafft <philipp.schafft@nacamar.de>
2021-02-27 08:32:50 +0000

    Feature: Added Stub for MP3 Decoder, and basic implementation of matching DecoderFactory

---
+++ src/main/java/io/ybrid/player/io/codec/mp3/Decoder.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2021 nacamar GmbH - Ybrid®, a Hybrid Dynamic Live Audio Technology
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package io.ybrid.player.io.codec.mp3;
+
+import io.ybrid.player.io.ByteDataSource;
+import io.ybrid.player.io.audio.PCMDataBlock;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
+
+public class Decoder implements io.ybrid.player.io.decoder.Decoder {
+    public Decoder(@NotNull ByteDataSource dataSource) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * Returns whether the state of this source is still valid.
+     *
+     * @return Whether this source is still valid.
+     */
+    @Override
+    public boolean isValid() {
+        return false;
+    }
+
+    @Override
+    public @NotNull PCMDataBlock read() throws IOException {
+        throw new IOException();
+    }
+
+    @Override
+    public void close() throws IOException {
+    }
+}
