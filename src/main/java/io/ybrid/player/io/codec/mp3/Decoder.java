8b7320e63b88831a4151d06f1dedbddaca44992a
Philipp Schafft <philipp.schafft@nacamar.de>
2021-02-27 09:23:36 +0000

    Feature: Implemented MP3 Decoder

--- src/main/java/io/ybrid/player/io/codec/mp3/Decoder.java
+++ src/main/java/io/ybrid/player/io/codec/mp3/Decoder.java
@@ -22,33 +22,73 @@
 
 package io.ybrid.player.io.codec.mp3;
 
+import io.ybrid.api.PlayoutInfo;
+import io.ybrid.api.metadata.Sync;
 import io.ybrid.player.io.ByteDataSource;
+import io.ybrid.player.io.MetadataInputStream;
 import io.ybrid.player.io.audio.PCMDataBlock;
+import javazoom.jl.decoder.Bitstream;
+import javazoom.jl.decoder.BitstreamException;
+import javazoom.jl.decoder.Header;
+import javazoom.jl.decoder.SampleBuffer;
+import org.jetbrains.annotations.NonNls;
 import org.jetbrains.annotations.NotNull;
 
 import java.io.IOException;
+import java.util.Objects;
+import java.util.logging.Logger;
 
 public class Decoder implements io.ybrid.player.io.decoder.Decoder {
+    private static final @NonNls Logger LOGGER = Logger.getLogger(Decoder.class.getName());
+    private final @NotNull MetadataInputStream inputStream;
+    private final Bitstream bitstream;
+    private final javazoom.jl.decoder.Decoder decoder;
+
     public Decoder(@NotNull ByteDataSource dataSource) {
-        throw new UnsupportedOperationException();
+        this.inputStream = new MetadataInputStream(dataSource);
+        this.bitstream = new Bitstream(this.inputStream);
+        this.decoder = new javazoom.jl.decoder.Decoder();
+        LOGGER.info("Decoder created");
     }
 
-    /**
-     * Returns whether the state of this source is still valid.
-     *
-     * @return Whether this source is still valid.
-     */
     @Override
     public boolean isValid() {
-        return false;
+        return true;
+    }
+
+    private static int getChannelCount(@NotNull Header header) {
+        return header.mode() == Header.SINGLE_CHANNEL ? 1 : 2;
     }
 
     @Override
     public @NotNull PCMDataBlock read() throws IOException {
-        throw new IOException();
+        final @NotNull Sync sync = Objects.requireNonNull(inputStream.getSync());
+        final PlayoutInfo playoutInfo = inputStream.getPlayoutInfo();
+        final @NotNull Header header;
+        final @NotNull SampleBuffer sampleBuffer;
+        final @NotNull short[] pcm;
+
+        try {
+            header = bitstream.readFrame();
+            sampleBuffer = (SampleBuffer) decoder.decodeFrame(header, bitstream);
+            pcm = sampleBuffer.getBuffer().clone();
+            bitstream.closeFrame();
+        } catch (Throwable e) {
+            LOGGER.warning("Decoder threw error: " + e);
+            throw new IOException(e);
+        }
+
+        return new PCMDataBlock(sync, playoutInfo, pcm, header.frequency(), getChannelCount(header));
     }
 
     @Override
     public void close() throws IOException {
+        LOGGER.info("Closing bitstream");
+        try {
+            bitstream.close();
+        } catch (BitstreamException e) {
+            LOGGER.warning("Closing bitstream failed: " + e);
+            throw new IOException(e);
+        }
     }
 }
